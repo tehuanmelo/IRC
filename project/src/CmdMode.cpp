9fe44e66a47cfa61e4806a51cf375f1736aa0995
tehuanmelo <tehuanmelo@gmail.com>
2024-05-27 14:03:18 +0400

    first commit to the new repo

--- project/src/CmdMode.cpp
+++ project/src/CmdMode.cpp
@@ -39,49 +39,73 @@
 // 21:24     /MODE +impsnkl secret_hideout 100
 // 21:24     /MODE mike +iw
 
-std::map<char, std::pair<int, std::string> > getModes(Channel channel, std::string modestring, std::vector<std::string> modeParams) {
+struct Mode {
+    int signal;
+    char mode;
+    std::string parameter;
+};
+
+struct ModeMessage {
+    std::string channel;
+    std::string modestring;
+    std::vector<std::string> modeParams;
+};
+
+std::vector<Mode> getModes(Channel& channel, ModeMessage& input) {
     
-    std::map<char, std::pair<int, std::string> > modes; // map to store the values the key is the mode 'x' the value is a pair<int,str> the int is the value to be set on/off the string is the mode parameter
+    std::vector<Mode> modes; // map to store the values the key is the mode 'x' the value is a pair<int,str> the int is the value to be set on/off the string is the mode parameter
     int flag; // variable to store the value on/off
     int paramIndex = 0;
+    Mode mod;
+
     
 
-    for (size_t i = 0; i < modestring.length(); i++) {
-            char c = modestring[i];
+    for (size_t i = 0; i < input.modestring.length(); i++) {
+            char c = input.modestring[i];
             if (c == '+')
                 flag = 1;
             else if (c == '-')
                 flag = 0;
             else if (c == 'k' || c == 'l') { // these two are special modes (key and limit) that require parameters
                 if (flag) { // if the mode is to set on a parameter is required
-                    if (!modeParams.empty()) {
+                    if (!input.modeParams.empty()) {
                         if (c == 'l') {
-                            int newLimit = std::atoi(modeParams[paramIndex].c_str());
-                            if (newLimit == 0 && modeParams[paramIndex] != "0") {
+                            int newLimit = std::atoi(input.modeParams[paramIndex].c_str());
+                            if (newLimit == 0 && input.modeParams[paramIndex] != "0") {
                                 paramIndex++;
                                 continue;
                             }
                         }
-                        modes[c] = std::make_pair(flag, modeParams[paramIndex++]); // paramIndex is used to track wich parameter was already taken, we get the value and increment it for the next
+                        mod.mode = c;
+                        mod.signal = flag;
+                        mod.parameter = input.modeParams[paramIndex++];
+                        modes.push_back(mod);
                     }
                 } 
                 else {
-                    if (channel.isModeSet(c) != flag)
-                        modes[c] = std::make_pair(flag, ""); // set the parameter value as empty string
+                    if (channel.isModeSet(c) != flag) {
+                        mod.mode = c;
+                        mod.signal = flag;
+                        mod.parameter = "";
+                        modes.push_back(mod);
+                    }
                 }
             } 
             else if (c == 'o') { // the mode operator requires parameter either for on and off
-                if (!modeParams.empty()) { // if there is parameter on the parameters vector
-                    if (channel.isUser(modeParams[paramIndex]))
-                        modes[c] = std::make_pair(flag, modeParams[paramIndex++]); // store the mode into the modes map
-                    else {
-                        paramIndex++;
-                    }
+                if (!input.modeParams.empty()) { // if there is parameter on the parameters vector
+                        mod.mode = c;
+                        mod.signal = flag;
+                        mod.parameter = input.modeParams[paramIndex++];
+                        modes.push_back(mod);
                 }
             }
             else if (c == 'i' || c == 't') {
-                if (channel.isModeSet(c) != flag)
-                    modes[c] = std::make_pair(flag, ""); // store them and the parameter is an empty string
+                if (channel.isModeSet(c) != flag) {
+                        mod.mode = c;
+                        mod.signal = flag;
+                        mod.parameter = "";
+                        modes.push_back(mod);
+                    }
             }
         
         }
@@ -89,22 +113,20 @@ std::map<char, std::pair<int, std::string> > getModes(Channel channel, std::stri
         return modes;
 }
 
-std::string getModestringSeted(std::map<char, std::pair<int, std::string> > modes) {
+std::string getModestringSeted(std::vector<Mode> modes) {
     std::string plusModes = "";
     std::string minusModes = "";
     std::string modesParams = "";
-    std::map<char, std::pair<int, std::string> >::iterator it;
-    for (it = modes.begin(); it != modes.end(); ++it) {
-        char c = it->first;
-        std::string str(1, c);
-        if (it->second.first == 1) {
+    for (size_t i = 0; i < modes.size(); i++) {
+        std::string str(1, modes[i].mode);
+        if (modes[i].signal == 1) {
             plusModes += str;
-            modesParams += it->second.second+" ";
+            modesParams += modes[i].parameter+" ";
         } 
         else {
             minusModes += str;
             if (str[0] == 'o')
-                modesParams += it->second.second+" ";
+                modesParams += modes[i].parameter+" ";
 
         }
     }
@@ -115,148 +137,150 @@ std::string getModestringSeted(std::map<char, std::pair<int, std::string> > mode
     return finalstring +" "+trimFunction(modesParams);
 }
 
-bool validateNumberModeParameters(std::string modestring, std::vector<std::string> modeParams) {
-    size_t counter = 0;
-    for (size_t i = 0; i < modestring.length(); i++) {
-        if (modestring[i] == 'k' || modestring[i] == 'l' || modestring[i] == 'o') {
-            counter++;
-        }
-    }
-    return counter > 0 ? counter == modeParams.size() : true;
-}
-
-void Server::modeCommand(ircMessage& msg, Client& user) 
-{
-    (void )user;
-    // ************************************************************* //
-    //  this block extracts the variables needed to set the modes
-    //  <channel> <modestring> [<parameters>]
-    // ************************************************************* //
-    std::string channel;
-    std::string modestring;
-    std::vector<std::string> modeParams;
+ModeMessage parseModeMessage(ircMessage& msg) {
+    ModeMessage newModes;
 
     for (size_t i = 0; i < msg.params.size(); i++) {
         if (i == 0)
-            channel = msg.params[i];
+            newModes.channel = msg.params[i];
         else if (i == 1)
-            modestring = msg.params[i];
+            newModes.modestring = msg.params[i];
         else            
-            modeParams.push_back(msg.params[i]);
+            newModes.modeParams.push_back(msg.params[i]);
     }
 
     if (!msg.trailing.empty()) {
         std::vector<std::string> tokens = split(msg.trailing, ' ');
         for (size_t i = 0; i < tokens.size(); i++) {
             if (i == 0) { // if is the first word from the trailing
-                modeParams.push_back(tokens[i].substr(1));  // remove the colom and store into the mode params
+                newModes.modeParams.push_back(tokens[i].substr(1));  // remove the colom and store into the mode params
                 continue;
             }
-            modeParams.push_back(tokens[i]);
+            newModes.modeParams.push_back(tokens[i]);
         }
     }
 
+    return newModes;
+}
 
-    // ************************************************************* //
-    // ************************************************************* //
-    // ************************************************************* //
-    //  this block is the first error handling layer
-    //  checks if the number of parameter is valid
-    //  if the channel exists
-    //  if the user has operator privileges
-    // ************************************************************* //
+bool validateModeString(ModeMessage input) {
+    size_t counter = 0;
+    int flag;
+
+    for (size_t i = 0; i < input.modestring.length(); i++) {
+        if (input.modestring[i] == '+')
+            flag = 1;
+        else if (input.modestring[i] == '-')
+            flag = 0;
+        
+        if (((input.modestring[i] == 'k' || input.modestring[i] == 'l') && flag == 1) || input.modestring[i] == 'o') {
+            std::cout << "this is the character: " << input.modestring[i] << std::endl;
+            counter++;
+        }
+    }
+    std::cout << "Number of modes: " << counter << std::endl;
+    std::cout << "Number of params: " << input.modeParams.size() << std::endl;
+    std::cout << (counter == input.modeParams.size()) << std::endl;
+    return counter == input.modeParams.size();
+}
+
+void Server::modeCommand(ircMessage& msg, Client& user) 
+{
+    // **************************************************** //
+    //  this is the first error handling block
+    //  checks if modesInput.channel wass passed and if
+    //  it is valid
+    // **************************************************** //
     std::string reply;
-    this->chan_it = this->getChannelIt(channel);
+
     if (msg.params.size() < 1) {
+        user.toSend.push_back(ERR_NEEDMOREPARAMS(user.nickname, "MODE"));
+        return;
+    }
+    else if (msg.params[0][0] != '#')
+    {
+        std::string target = msg.params[0];
+        if(this->isUserNick(target))
+        {
+            if(msg.params.size() > 1)
+                reply = ERR_UMODEUNKNOWNFLAG(user.nickname);
+            else
+                reply =  RPL_CHANNELMODEIS(user.nickname,"", "");
+        }
+        else
+            reply = ERR_USERSDONTMATCH(user.nickname);
+        user.toSend.push_back(reply);
+        return;
+    }
+    // **************************************************** //
+
+
+    ModeMessage modesInput = parseModeMessage(msg);
+    this->chan_it = this->getChannelIt(modesInput.channel);
+    
+    
+    if (this->chan_it == this->channels.end()) {
+        reply = ERR_NOSUCHCHANNEL(user.nickname, modesInput.channel);
+    } else if (!validateModeString(modesInput)) {
         reply = ERR_NEEDMOREPARAMS(user.nickname, "MODE");
     }
-    // else if (msg.params[0][0] != '#')
-    // {
-    //     std::string target = msg.params[0];
-    //     if(this->isUserNick(target))
-    //     {
-    //         if(msg.params.size() > 1)
-    //             reply = ERR_UMODEUNKNOWNFLAG(user.nickname);
-    //         else
-    //             reply =  RPL_CHANNELMODEIS(user.nickname,"", "");
-    //     }
-    //     else
-    //         reply = ERR_USERSDONTMATCH(user.nickname);
-    // }
-    else if (chan_it == this->channels.end()) {
-        reply = ERR_NOSUCHCHANNEL(user.nickname, channel);
-    }
-    else if (modestring.find_first_not_of("+-kloit") != std::string::npos) {
-        std::size_t charPosition = modestring.find_first_not_of("+-kloit");
-        char c = modestring[charPosition];
+    else if (modesInput.modestring.find_first_not_of("+-kloit") != std::string::npos) {
+        std::size_t charPosition = modesInput.modestring.find_first_not_of("+-kloit");
+        char c = modesInput.modestring[charPosition];
         std::string charString(1, c);
         reply = ERR_UNKNOWNMODE(user.nickname, charString);
     }
-    else if (msg.params.size() == 1 && this->channels[channel].isUser(user)) {
-        reply = RPL_CHANNELMODEIS(user.nickname, channel, this->channels[channel].getModes());
+    else if (msg.params.size() == 1 && this->chan_it->second.isUser(user)) {
+        reply = RPL_CHANNELMODEIS(user.nickname, modesInput.channel, this->chan_it->second.getModes());
     }
     else if (msg.params.size() > 1 && !chan_it->second.isOperator(user)) {
-        reply = ERR_CHANOPRIVSNEEDED(user.nickname, channel);
+        reply = ERR_CHANOPRIVSNEEDED(user.nickname, modesInput.channel);
     }
-    else {
+    else { // if no error detected execute modes
 
-        std::map<char, std::pair<int, std::string> > modes;
-        this->chan_it = this->getChannelIt(channel);
-        if (this->chan_it != this->channels.end())
-            modes = getModes(chan_it->second, modestring, modeParams);
+        std::vector<Mode> modes;
+        if (this->chan_it != this->channels.end()) {
+            modes = getModes(this->chan_it->second, modesInput);
 
-        std::map<char, std::pair<int, std::string> >::iterator it;
-        for (it = modes.begin(); it != modes.end(); ++it) {
-
-            int flag = it->second.first;
-            std::string parameter = it->second.second;
-            char mode = it->first;
-
-            if (mode == 'l' && flag) {
-                    this->channels[channel].setUserLimit(std::atoi(parameter.c_str()));
-                    std::cout << "user limit set: " << this->channels[channel].getUserLimit() << std::endl;
-            } 
-            else if (mode == 'k' && flag) {
-                this->channels[channel].setPassword(parameter);
-                std::cout << "password set: " << this->channels[channel].getPassword() << std::endl;
-            } 
-            else if (mode == 'o') {
-                Client* client = this->getUser(parameter);
-                    if (client != NULL) {
-                        if (client &&this->channels[channel].isUser(*client)) {
-                            if (flag == 1) {
-                                if (!this->channels[channel].isOperator(*client)) {
-                                    this->channels[channel].addOperator(*client);
+            for (size_t i = 0; i < modes.size(); ++i) {
+                if (modes[i].mode == 'l' && modes[i].signal) {
+                        this->chan_it->second.setUserLimit(std::atoi(modes[i].parameter.c_str()));
+                } 
+                else if (modes[i].mode == 'k' && modes[i].signal) {
+                    this->chan_it->second.setPassword(modes[i].parameter);
+                } 
+                else if (modes[i].mode == 'o') {
+                    Client* client = this->getUser(modes[i].parameter);
+                        if (client != NULL) {
+                            if (client && this->chan_it->second.isUser(*client)) {
+                                if (modes[i].signal == 1) {
+                                    if (!this->chan_it->second.isOperator(*client)) {
+                                        this->chan_it->second.addOperator(*client);
+                                        std::cout << "inside add operator mode client name " << client->nickname << std::endl;
+                                    }
                                 }
-                                else
-                                    std::cout << client->nickname << " is already an operator of " << channel << std::endl;
-                            }
-                            else if (flag == 0) {
-                                if (this->channels[channel].isOperator(*client)) {
-                                    this->channels[channel].removeOperator(*client);
+                                else if (modes[i].signal == 0) {
+                                    if (this->chan_it->second.isOperator(*client)) {
+                                        this->chan_it->second.removeOperator(*client);
+                                    }
                                 }
-                                else
-                                    std::cout << client->nickname << " is not an operator of " << channel << std::endl;
-                            }
-                        } 
-                        else
-                            std::cout << client->nickname << " is not an user of " << channel << std::endl;
-                    }
-                    else
-                        std::cout << parameter << " is not an user in the server " << std::endl;
-                continue;
+                            } 
+                        }
+                    continue;
+                }
+                this->chan_it->second.setMode(modes[i].mode, modes[i].signal);      // /mode +ti
             }
-            
-            
+           
 
-            this->channels[channel].setMode(mode, it->second.first);      // /mode +ti
+        std::string reply = MODE_CHANNELCHANGEMODE(user_id(user.nickname, user.username), modesInput.channel, getModestringSeted(modes));
+        this->channels[modesInput.channel].broadcast(reply);
+        return;
+
         }
 
-        if (!modes.empty()) {
-            std::string reply = MODE_CHANNELCHANGEMODE(user_id(user.nickname, user.username), channel, getModestringSeted(modes));
-            this->channels[channel].broadcast(reply);
-            return;
-        }
+
+       
+
     }
 
     user.toSend.push_back(reply);
